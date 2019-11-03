72b13702d6b8ed4c6f1724d42f526d55267b68fe
Ellissquires <esquires1@sheffield.ac.uk>
2019-11-03 00:30:42 +0000

    Daver v2

--- daver/envs/daver_env.py
+++ daver/envs/daver_env.py
@@ -10,15 +10,9 @@ class DaverEnv(gym.Env):
     def __init__(self):
         self.width = 10
         self.height = 10
-
-        self.pos = np.array([0, 0])
-        self.state = np.zeros((self.width,self.height))
-        self.state[self.pos[0]][self.pos[1]] = 1
-        self.state[4][4] = 2
-
-        self.safe_location = np.array([4,4])
-
         self.action_space = spaces.Discrete(4)
+
+        self.reset()
 
     def step(self, action):
         if action == 0:
@@ -35,13 +29,16 @@ class DaverEnv(gym.Env):
                 self.pos[0] -= 1
         
         s_ = self.pos
-
-        if np.array_equal(s_, self.safe_location):
-            reward = 20
+        self.enemy_move()
+        if np.array_equal(s_, self.enemy_pos):
+            reward = -100000
             done = True
+        elif np.array_equal(s_, self.safe_location):
+            reward = 1
+            done = True 
         else:
-            reward = -1
-            done = False 
+            reward = 0
+            done = False
         
         self.state = np.zeros((self.width,self.height))
         self.state[self.pos[0]][self.pos[1]] = 1
@@ -54,18 +51,33 @@ class DaverEnv(gym.Env):
     
     def render(self):
         self.state = np.zeros((self.width,self.height))
-        self.state[self.pos[0]][self.pos[1]] = 1
-        self.state[4][4] = 2        
+        self.state[self.pos[0]][self.pos[1]] = 2
+        self.state[self.enemy_pos[0]][self.enemy_pos[1]] = 1
         print(self.state)
+
+    def enemy_move(self):
+        if not np.array_equal(self.pos, self.enemy_pos):
+            diff = self.enemy_pos - self.pos 
+            if abs(diff[0]) > abs(diff[1]):
+                self.enemy_pos[0] -= diff[0] / abs(diff[0])
+            else:
+                self.enemy_pos[1] -= diff[1] / abs(diff[1])
+
 
 
 
     def reset(self):
         self.state = np.zeros((self.width,self.height))
-        random_pos = np.random.randint(10, size=2)
-        self.state[random_pos[0]][random_pos[1]] = 1
-        self.state[4][4] = 2 
-        self.pos = random_pos
+        random_pos1 = np.random.randint(10, size=2)
+        random_pos2 = np.random.randint(10, size=2)
+        random_pos3 = np.random.randint(10, size=2)
+
+        self.state[random_pos1[0]][random_pos1[1]] = 1
+        self.state[random_pos2[0]][random_pos2[1]] = 2
+        self.state[random_pos3[0]][random_pos3[1]] = 3
+        self.enemy_pos = random_pos1
+        self.pos = random_pos2
+        self.safe_location = random_pos3
 
 
         return 0     

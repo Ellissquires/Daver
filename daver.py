ce043095bd230132af2a66111a4f7c5878551636
Ellissquires <esquires1@sheffield.ac.uk>
2019-11-03 10:04:02 +0000

    Fix my complete and utter mess from earlier

--- daver.py
+++ daver.py
@@ -3,6 +3,7 @@ import random
 from IPython.display import clear_output
 from collections import deque
 import progressbar
+import json
 
 import gym
 import daver
@@ -13,7 +14,7 @@ from tensorflow.keras.optimizers import Adam
 
 env = gym.envs.make("daver-v0")
 
-q_table = np.zeros([100, 4])
+q_table = np.zeros([10000, 4])
 
 import random
 from IPython.display import clear_output
@@ -26,9 +27,10 @@ epsilon = 0.1
 # For plotting metrics
 all_epochs = []
 all_penalties = []
-
+current_frame_dict = {}
 pens = [0]
-for i in range(1, 100001):
+
+for i in range(1, 10001):
     state = env.reset()
 
     epochs, penalties, reward, = 0, 0, 0
@@ -44,48 +46,76 @@ for i in range(1, 100001):
 
         old_value = q_table[state, action]
         next_max = np.max(q_table[next_state])
-        
         new_value = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)
         q_table[state, action] = new_value
 
-        if reward == -1:
-            penalties += 1
-
         state = next_state
         epochs += 1
-    
-    pens[-1] += penalties
         
     if i % 100 == 0:
-        pens.append(0)
-        clear_output(wait=True)
+        all_epochs.append(epochs)
         print(f"Episode: {i}")
 
 print("Training finished.\n")
+print(all_epochs)
+
+
+# export = json.dumps(current_frame_dict)
+# print(current_frame_dict)
+# current_frame_dict = { k: v.tolist() for k, v in current_frame_dict.items() }
+
+
+
+# export = json.dumps(current_frame_dict)
+# print(current_frame_dict)
+# current_frame_dict = { k: v.tolist() for k, v in current_frame_dict.items() }
+with open('export.json', 'w') as json_file:
+    json.dump(current_frame_dict, json_file, indent=4)
 
 """Evaluate agent's performance after Q-learning"""
 
-print(pens)
 total_epochs, total_penalties = 0, 0
-episodes = 100
-
-for _ in range(episodes):
+episodes = 1000
+max_move = 100
+times_caught = 0
+safe_deposited = 0
+safe = []
+for e in range(episodes):   
     state = env.reset()
     epochs, penalties, reward = 0, 0, 0
     
     done = False
-    
+    i = 0
     while not done:
         action = np.argmax(q_table[state])
         state, reward, done = env.step(action)
-        if reward == -1:
-            penalties += 1
+        # if reward == -1000:
+        #     times_caught += 1
+        if reward == 5:
+            safe.append(e)
+            safe_deposited += 1
 
+        if (i == max_move):
+            done = True
+
+        i+=1
+        env.render()
+        if e in current_frame_dict:
+            current_frame_dict[e].append(env.state.tolist())
+        else:
+            current_frame_dict[e] = [env.state.tolist()]
+        
         epochs += 1
 
     total_penalties += penalties
     total_epochs += epochs
 
+with open('export.json', 'w') as json_file:
+    json.dump(current_frame_dict, json_file, indent=4)
+
 print(f"Results after {episodes} episodes:")
 print(f"Average timesteps per episode: {total_epochs / episodes}")
-print(f"Average penalties per episode: {total_penalties / episodes}")
+# print(f"Average penalties per episode: {total_penalties / episodes}")
+print("Agent caught " + str(times_caught) + " times")
+print("Agent deposited " + str(safe_deposited) + " times")
+print(safe)
--- daver/envs/daver_env.py
+++ daver/envs/daver_env.py
@@ -10,15 +10,9 @@ class DaverEnv(gym.Env):
     def __init__(self):
         self.width = 10
         self.height = 10
-
-        self.pos = np.array([0, 0])
-        self.state = np.zeros((self.width,self.height))
-        self.state[self.pos[0]][self.pos[1]] = 1
-        self.state[4][4] = 2
-
-        self.safe_location = np.array([4,4])
-
         self.action_space = spaces.Discrete(4)
+
+        self.reset()
 
     def step(self, action):
         if action == 0:
@@ -35,37 +29,61 @@ class DaverEnv(gym.Env):
                 self.pos[0] -= 1
         
         s_ = self.pos
-
+        self.enemy_move()
         if np.array_equal(s_, self.safe_location):
-            reward = 20
+            reward = 5
             done = True
+        # elif np.array_equal(s_, self.safe_location):
+        #     reward = 1000
+        #     done = True 
         else:
             reward = -1
-            done = False 
+            done = False
         
         self.state = np.zeros((self.width,self.height))
         self.state[self.pos[0]][self.pos[1]] = 1
-        self.state[4][4] = 2        
-
-
-        encoded = self.pos[0] * 10 + self.pos[1]
+        self.state[self.enemy_pos[0]][self.enemy_pos[1]] = 2
+        
+        encoded = self.pos[0] + self.pos[1] * 10 + self.enemy_pos[0] * 100 + self.enemy_pos[1] * 1000 
         return encoded, reward, done
 
     
     def render(self):
         self.state = np.zeros((self.width,self.height))
-        self.state[self.pos[0]][self.pos[1]] = 1
-        self.state[4][4] = 2        
+        self.state[self.safe_location[0]][self.safe_location[1]] = 3
+        self.state[self.pos[0]][self.pos[1]] = 2
+        self.state[self.enemy_pos[0]][self.enemy_pos[1]] = 1
+        
         print(self.state)
 
+    def enemy_move(self):
+        if not np.array_equal(self.pos, self.enemy_pos):
+            if np.random.uniform(0, 1) > 0.9:
+                self.enemy_pos += np.random.randint(1, size=2)
+            else:
+                diff = self.enemy_pos - self.pos 
+                if abs(diff[0]) > abs(diff[1]):
+                    self.enemy_pos[0] -= diff[0] / abs(diff[0])
+                else:
+                    self.enemy_pos[1] -= diff[1] / abs(diff[1])
 
 
+
+    def get_frame(self):
+        return self.state
+        
     def reset(self):
         self.state = np.zeros((self.width,self.height))
-        random_pos = np.random.randint(10, size=2)
-        self.state[random_pos[0]][random_pos[1]] = 1
-        self.state[4][4] = 2 
-        self.pos = random_pos
+        random_pos1 = np.random.randint(10, size=2)
+        random_pos2 = np.random.randint(10, size=2)
+        random_pos3 = np.random.randint(10, size=2)
+
+        self.state[random_pos1[0]][random_pos1[1]] = 1
+        self.state[random_pos2[0]][random_pos2[1]] = 2
+        self.state[random_pos3[0]][random_pos3[1]] = 3
+        self.enemy_pos = random_pos1
+        self.pos = random_pos2
+        self.safe_location = random_pos3
 
 
         return 0     
